11b71852bb8771f4bd28eb64fa25ce3bee02f6a8
JoseLuisOrtega <jlop9810@gmail.com>
2025-01-29 20:07:18 +0100

    Ejercicio T4

---
+++ codi/03-javascript/ejercicios_js/T4/js/formulari.js
@@ -0,0 +1,18 @@
+const formulario = document.firstContact;
+const nombre = document.getElementById("nombre");
+const telef = document.getElementById("telef");
+const dia = document.getElementById("dia");
+const mes = document.getElementById("mes");
+const ano = document.getElementById("ano");
+const correo = document.getElementById("correo");
+const dni = document.getElementById("dni");
+const condiciones = document.getElementById("condiciones");
+
+formulario.addEventListener("submit", (e) => {
+  const nom = validaNom;
+  const telf = validaTelf;
+  const date = validaDate;
+  const email = validaMail;
+  const dni = validaDNI;
+  const cond = validaCond;
+});
