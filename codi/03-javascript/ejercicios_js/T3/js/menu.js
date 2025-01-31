240862f968e6761301accbcf90ab0109cbb38aab
JoseLuisOrtega <jlop9810@gmail.com>
2025-01-31 20:13:53 +0100

    Acabando ejercicios menu

--- codi/03-javascript/ejercicios_js/T3/js/menu.js
+++ codi/03-javascript/ejercicios_js/T3/js/menu.js
@@ -1,8 +1,31 @@
 const mostra6 = document.getElementById("mostra6");
 const btn6 = document.getElementById("btn_ejer6");
 const limpiar = document.getElementById("btn_limpiar6");
+const tabla = document.getElementById("mostramenu");
 let primeros = ["Ensalada", "Sopa", "Escalivada", "Paella", "Macarrones"];
 let segundos = ["Entrecot", "Pescado", "Estofado", "Canelones", "Hamburguesa"];
 let postres = ["Pastel", "Yogurt", "Manzana", "Helado", "Na de na"];
-let menu =
-  "<h2>Menu Semanal<h2> <br/>" + '<h3 class= "primeros_platos">Primeros Platos';
+let dias = [
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo",
+];
+
+btn6.onclick = () => {
+  let row = document.createElement("tr");
+  row.appendChild(document.createElement("th"));
+  row.appendChild(
+    document.createElement("th").appendChild(document.createTextNode("Primero"))
+  );
+  row.appendChild(
+    document.createElement("th").appendChild(document.createTextNode("Segundo"))
+  );
+  row.appendChild(
+    document.createElement("th").appendChild(document.createTextNode("Postre"))
+  );
+  tabla.appendChild(row);
+};
